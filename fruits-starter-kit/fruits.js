1c707e5b80630438ac100492f74a146fa0385354
Corentin <Corentin.Lanaud@wanadoo.fr>
2024-12-04 15:48:46 +0100

    hover fini

--- fruits-starter-kit/fruits.js
+++ fruits-starter-kit/fruits.js
@@ -7,7 +7,7 @@ let fruits = [{ name: "Orange", image: "orange.jpg" },
 
 let button = document.getElementById("fruits");
 let imageContainer = document.getElementById("imageFruit");
-
+let clickedButton = null;
 
 
 fruits.forEach((fruit) => {
@@ -20,21 +20,27 @@ fruits.forEach((fruit) => {
         let allButtons = document.querySelectorAll('.boutonFruits');
         allButtons.forEach(btn => {
             btn.style.backgroundColor = 'gray';
+            
         });
         div.style.backgroundColor = "red"
         imageContainer.style.backgroundImage = `url(${fruit.image})`;
+        clikedButton = fruit;
     });
 
     div.addEventListener("mouseout", () => {
         let allButtons = document.querySelectorAll('.boutonFruits');
-
+        if (clikedButton){
+            imageContainer.style.backgroundImage = `url(${clickedButton.image})`;
+        }
+        
+        
     });
 
     div.addEventListener("mouseover", () => {
         let allButtons = document.querySelectorAll('.boutonFruits');
-
+        imageContainer.style.backgroundImage = `url(${fruit.image})`;
     });
-
+    
 
     button.appendChild(div);
 });
