d2c71a7ce0219ad22d21b42fcae728b335f4e56b
Corentin <Corentin.Lanaud@wanadoo.fr>
2024-12-05 09:36:12 +0100

    fini

--- fruits-starter-kit/fruits.js
+++ fruits-starter-kit/fruits.js
@@ -7,9 +7,7 @@ let fruits = [{ name: "Orange", image: "orange.jpg" },
 
 let button = document.getElementById("fruits");
 let imageContainer = document.getElementById("imageFruit");
-let imageContainer2 = document.querySelector('.imageChanger');
-let currentImage = getComputedStyle(imageContainer2).backgroundImage;
-
+let SelectedImage = "";
 
 fruits.forEach((fruit) => {
     console.log(fruit.name);
@@ -24,31 +22,24 @@ fruits.forEach((fruit) => {
             
         });
         div.style.backgroundColor = "red"
-        imageContainer.style.backgroundImage = `url(${fruit.image})`;
+        SelectedImage = `url(${fruit.image})`;
+        imageContainer.style.backgroundImage = SelectedImage;
         
     });
 
     div.addEventListener("mouseout", () => {
-        let allButtons = document.querySelectorAll('.boutonFruits');
-        if (!div.style.backgroundColor || div.style.backgroundColor === 'gray') {
-            imageContainer.style.backgroundImage = currentImage;
-        }
+        
+        imageContainer.style.backgroundImage = SelectedImage ;
         
         
     });
 
     div.addEventListener("mouseover", () => {
-        let allButtons = document.querySelectorAll('.boutonFruits');
         imageContainer.style.backgroundImage = `url(${fruit.image})`;
     });
 
     
 
     button.appendChild(div);
-});
 
-imageContainer.addEventListener("mouseleave", () => {
-    if (!clickedFruit) {
-        imageContainer.style.backgroundImage = `url(${defaultImage})`;
-    }
 });
